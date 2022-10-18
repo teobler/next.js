553e31c98009b1e99f7d8dfa8489d999624a18b3
teobler <teobler@163.com>
2022-10-18 15:30:10 +0800

    test: change the order of scripts in test to make test more reasonable(#41282).

--- test/integration/script-loader/base/pages/page11.js
+++ test/integration/script-loader/base/pages/page11.js
@@ -4,10 +4,10 @@ const Page = () => {
   return (
     <div className="container">
       <Script
-        id="id-1"
-        order={0}
+        id="id-4"
         strategy="beforeInteractive"
-        src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js?a=2"
+        order={3}
+        dangerouslySetInnerHTML={{ __html: `() => console.log(4)` }}
       />
       <Script
         id="id-2"
@@ -16,17 +16,18 @@ const Page = () => {
         dangerouslySetInnerHTML={{ __html: `() => console.log(2)` }}
       />
       <Script
+        id="id-1"
+        order={0}
+        strategy="beforeInteractive"
+        src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js?a=2"
+      />
+      <Script
         id="id-3"
         strategy="beforeInteractive"
         order={2}
         src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js?a=1"
       />
-      <Script
-        id="id-4"
-        strategy="beforeInteractive"
-        order={3}
-        dangerouslySetInnerHTML={{ __html: `() => console.log(4)` }}
-      />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js?a=3" />
       <div>Page 11</div>
     </div>
   )
